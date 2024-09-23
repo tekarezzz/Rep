4109407284b3751d9bf5cd07d44a0696ae39d63c
Dmitry Koshelev <teleredmi4@gmail.com>
2024-09-23 17:14:22 +0500

    Задание 1 выполнено

---
+++ task0/task0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Судьба");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Ты говоришь: «Она не стоит свеч,");
+            Console.WriteLine("Игра судьбы. Темны её сплетенья».");
+            Console.WriteLine("Но не бывает лишних встреч,");
+            Console.WriteLine("И неслучайны совпаденья.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
