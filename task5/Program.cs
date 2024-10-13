708355f2f5d08c941ab4de66352e8fa2f721762c
Dmitry Koshelev <teleredmi4@gmail.com>
2024-10-13 22:34:39 +0500

    Задание 5 выполнено.

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(3, 11) + F(5, 13) + F(7, 17);
+            Console.WriteLine(Math.Round(x, 3));
+            Console.ReadKey();
+        }
+
+
+        static double F(double a, double b)
+        {
+            return (1 + (a * a)) / (1 + b * b);
+        }
+    }
+}
