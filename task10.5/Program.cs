1843ba17a023032ddc4c1533cd294ac9c873c9ce
Dmitry Koshelev <teleredmi4@gmail.com>
2024-12-24 21:45:25 +0500

    Задание 10.5 выполнено. 11 вариант

---
+++ task10.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите начальный пробег n (км): ");
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите процент увеличения m: ");
+        double m = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите максимальный пробег k (км): ");
+        double k = Convert.ToDouble(Console.ReadLine());
+
+        double totalDistance = 0;
+        int day = 0;
+
+        while (totalDistance <= k)
+        {
+            day++;
+            totalDistance += n;
+            n += n * (m / 100);
+        }
+
+        Console.WriteLine($"Суммарный пробег превысит {k} км на {day}-й день.");
+        Console.ReadKey();
+    }
+}
