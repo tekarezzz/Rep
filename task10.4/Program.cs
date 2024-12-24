c8cfc9ac4381ed5aeba361185a211c3188bc230a
Dmitry Koshelev <teleredmi4@gmail.com>
2024-12-24 21:41:25 +0500

    Задание 10.4 выполнено. 11 вариант

---
+++ task10.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число (все цифры различны): ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int minDigit = 10;
+        int position = 0;
+        int index = 1;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+
+            if (digit < minDigit)
+            {
+                minDigit = digit;
+                position = index;
+            }
+
+            number /= 10;
+            index++;
+        }
+
+        Console.WriteLine($"Порядковый номер минимальной цифры: {position}");
+        Console.ReadKey();
+    }
+}
