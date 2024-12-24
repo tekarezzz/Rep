daf8d0d766b85f9fa6012d3ece9fd7d9aea78df6
Dmitry Koshelev <teleredmi4@gmail.com>
2024-12-24 21:27:05 +0500

    Задание 10.2 выполнено. 11 вариант

---
+++ task10.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество административных единиц: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double totalPopulation = 0;
+        double totalArea = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"Введите количество жителей (в тыс. чел.) для единицы {i}: ");
+            double population = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"Введите площадь (в кв. км) для единицы {i}: ");
+            double area = Convert.ToDouble(Console.ReadLine());
+
+            totalPopulation += population;
+            totalArea += area;
+        }
+
+        double averageDensity = totalPopulation / totalArea;
+        Console.WriteLine($"Средняя плотность населения: {averageDensity} тыс. чел. на кв. км");
+
+        Console.ReadKey();
+    }
+}
