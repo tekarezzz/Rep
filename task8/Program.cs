4a1859a56beeb616eb4f1b108269ed661ae8dcd6
Dmitry Koshelev <teleredmi4@gmail.com>
2024-12-24 20:28:41 +0500

    Задание 8 выполнено. 11 вариант

---
+++ task8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента функции");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x}) = {MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+
+
+        static double MyFunction(double x)
+        {
+            if (Math.Abs(x) < Math.PI / 4)
+                return Math.Atan(x);
+            else if (x >= Math.PI / 4)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
